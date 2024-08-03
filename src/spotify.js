2ab2b040846b645267d33ead584f430d38943883
Anurag059 <anuragtripathi059@gmail.com>
2024-08-03 18:17:31 +0530

    Update spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
-const clientId = "7afbf1411b2b4ad6a154109c16ba7656";
+const clientId = "10ff71b2181a4ee19909431870ef03f4";
 const redirectUri = "http://localhost:3000";
 const scopes = ["user-library-read", "playlist-read-private"];
 
@@ -10,14 +10,12 @@ export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=
 )}&response_type=token&show_dialog=true`;
 
 const apiClient = axios.create({
-  baseURL: "https://api.spotify.com/v1",
+  baseURL: "https://api.spotify.com/v1/",
 });
 
 export const setClientToken = (token) => {
-  apiClient.interceptors.request.use((config) => {
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  apiClient.interceptors.request.use(async function (config) {
+    config.headers.Authorization = "Bearer " + token;
     return config;
   });
 };
